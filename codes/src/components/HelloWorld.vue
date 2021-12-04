948fc076b4d59c7767f6f3f49e7d7be3daa0f431
Milton Bugallo <79487438+miltonbugallo@users.noreply.github.com>
2021-12-04 19:59:29 -0300

    Add files via upload

---
+++ codes/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mt-3">
+    <div class="card" style="width: 100%;">
+      <div class="card-body text-center bg-primary text-light">
+        <h5 class="card-title">Gifs & Stickers</h5>
+        <p class="card-text">
+          Esta aplicación es parte del curso de Vue 3 en Udemy. Se integró Bootstrap, se trabajó con rutas y muchas cosas
+          más.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ codes/src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container mt-3">
+    <div class="card" style="width: 100%;">
+      <div class="card-body text-center bg-primary text-light">
+        <h5 class="card-title">About</h5>
+        <h6 class="card-subtitle mb-2">Hecho por Milton Bugallo</h6>
+        <p class="card-text">
+          Esta aplicación utiliza el API de
+          <a class="text-light" href="http://giphy.com" target="_blank" rel="noopener noreferrer"
+            >giphy.com</a
+          >
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
